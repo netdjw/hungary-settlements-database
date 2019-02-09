b917cf54ba92ae4449fb41852b9365c738fd63bc
Winkler Balázs <djw@djw.hu>
2019-02-09 11:37:21 +0100

    v1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# What is this?
+
+This is a country and settlement database of Hungary.
+
+# Formats
+
+- CSV
+- ODS
+- Laravel database seeder
+
+# Feel free to use
+
+This collection is absolutly free to use to any projects.
+
+# Updates
+
+If you want to supplement or update the database files fell free to create a pull request or an issue.
---
+++ laravel-database-seeders/CountrySeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Country extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('countries')->insert([
+            ['id' => 1, 'name' => 'Budapest'],
+            ['id' => 2, 'name' => 'Baranya'],
+            ['id' => 3, 'name' => 'Bács-Kiskun'],
+            ['id' => 4, 'name' => 'Békés'],
+            ['id' => 5, 'name' => 'Borsod-Abaúj-Zemplén'],
+            ['id' => 6, 'name' => 'Csongrád'],
+            ['id' => 7, 'name' => 'Fejér'],
+            ['id' => 8, 'name' => 'Győr-Moson-Sopron'],
+            ['id' => 9, 'name' => 'Hajdú-Bihar'],
+            ['id' => 10, 'name' => 'Heves'],
+            ['id' => 11, 'name' => 'Komárom-Esztergom'],
+            ['id' => 12, 'name' => 'Nógrád'],
+            ['id' => 13, 'name' => 'Pest'],
+            ['id' => 14, 'name' => 'Somogy'],
+            ['id' => 15, 'name' => 'Szabolcs-Szatmár-Bereg'],
+            ['id' => 16, 'name' => 'Jász-Nagykun-Szolnok'],
+            ['id' => 17, 'name' => 'Tolna'],
+            ['id' => 18, 'name' => 'Vas'],
+            ['id' => 19, 'name' => 'Veszprém'],
+            ['id' => 20, 'name' => 'Zala'],
+        ]);
+    }
+}
